9d405dfd2ffc812feeb75cc7fec0c3ead9c645b9
lintang <xyz@xyz>
2019-06-17 22:32:21 +0700

    first commit

---
+++ MoveIntent/app/src/main/java/com/example/moveintent/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.moveintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button moveIntent, moveWithData, moveWithObject, dialNum, moveWithResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initial();
+
+        moveIntent.setOnClickListener(this);
+        moveWithData.setOnClickListener(this);
+        moveWithObject.setOnClickListener(this);
+        dialNum.setOnClickListener(this);
+        moveWithResult.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_move :
+                Intent intent = new Intent(this,MoveActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void initial(){
+        moveIntent = findViewById(R.id.btn_move);
+        moveWithData = findViewById(R.id.btn_move_with_data);
+        moveWithObject = findViewById(R.id.btn_move_with_object);
+        dialNum = findViewById(R.id.btn_phone);
+        moveWithResult = findViewById(R.id.activity_with_result);
+
+    }
+}
