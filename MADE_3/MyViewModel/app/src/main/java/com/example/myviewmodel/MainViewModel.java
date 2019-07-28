68098804086c789006e0096e75f33c2a3e19a24f
lintang <xyz@xyz>
2019-07-28 23:21:37 +0700

    update

---
+++ MADE_3/MyViewModel/app/src/main/java/com/example/myviewmodel/MainViewModel.java
@@ -0,0 +1,55 @@
+package com.example.myviewmodel;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainViewModel extends ViewModel {
+
+    private static final String API_KEY = "be56210a15edc10c732bbb2a9a355877";
+    private MutableLiveData<ArrayList<WeatherItems>> listWeathers = new MutableLiveData<>();
+
+    void setWeather(final String cities) {
+        // request API
+        AsyncHttpClient client = new AsyncHttpClient();
+        final ArrayList<WeatherItems> listItems = new ArrayList<>();
+        String url = "https://api.openweathermap.org/data/2.5/group?id=" + cities + "&units=metric&appid=" + API_KEY;
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("list");
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject weather = list.getJSONObject(i);
+                        WeatherItems weatherItems = new WeatherItems(weather);
+                        listItems.add(weatherItems);
+                    }
+                    listWeathers.postValue(listItems);
+                } catch (Exception e) {
+                    Log.d("Exception", e.getMessage());
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", error.getMessage());
+            }
+        });
+    }
+    LiveData<ArrayList<WeatherItems>> getWeathers() {
+        return listWeathers;
+    }
+}
+
